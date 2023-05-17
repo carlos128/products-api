f84b378134d80c1d15c7998832d6f4aefb396d1a
carlos <gcralos@ymail.com>
2023-05-16 21:28:02 -0500

    feat:construction: CH-2345: Se renombra la clase

--- src/contabilida/Contabilid.ts
+++
@@ -1,8 +0,0 @@
-import { IContabilida } from "./IContabilida";
-
-export class  Contabilida  implements IContabilida{
-    getBusinessActivete(pasivo: number, activo: number): number {
-        return pasivo + activo;
-    }
-    
-}
