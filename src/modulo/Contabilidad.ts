61e37ed68f2021e292333e0d73a6d834f30e3d9e
carlos <gcralos@ymail.com>
2023-05-17 18:38:21 -0500

    feat:construction: CH-4567:Se renombra la importacion de la interface

--- src/modulo/Contabilidad.ts
+++ src/modulo/Contabilidad.ts
@@ -1,4 +1,4 @@
-import { Icontable } from "./IContable";
+import { Icontable } from "./IContabilidad";
 
 export class Contabilidad  implements  Icontable{
     getBusinessCapital(pasivos: number, activos: number): number {
