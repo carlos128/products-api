795d2a013c750850130d59738efd58d08507836e
carlos <gcralos@ymail.com>
2023-05-18 05:16:55 -0500

    feat:construction: CH-8907  se elimina la carpeta  modules

--- src/modulo/Contabilidad.ts
+++
@@ -1,9 +0,0 @@
-import { Icontable } from "./IContabilidad";
-
-export class Contabilidad  implements  Icontable{
-    getBusinessCapital(pasivos: number, activos: number): number {
-        return  pasivos+ activos;
-    }
-
-
-}
